143e97fdc554cd4d87263b50ed53f5d3b19e11ea
n48743rj <rohan.joshi@student.manchester.ac.uk>
2022-01-17 18:26:51 +0000

    mainpage php file created

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+
+include "header.php";
+
+?>
+
+<html>
+<head>
+    <title>PHP Quiz Application</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="User can take a quiz on this page">
+    <meta charset="utf-8">
+    <script src="myScript.js"></script>
+</head>
+
+<body>
+    <?php $current_time = time(); echo $current_time ?>
+</body>
+
+
+</html>
